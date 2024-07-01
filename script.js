cb63527f8537835639a5791ec6ef7ae44c5e5273
Meelome <80218429+Meelome@users.noreply.github.com>
2024-07-01 16:12:27 +0300

    Update .1

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 let clickCount = 0;
+let canClick = true;
 
 document.getElementById('cookie').addEventListener('click', () => {
-    clickCount++;
-    document.getElementById('clickCount').textContent = clickCount;
+    if (canClick) {
+        clickCount++;
+        document.getElementById('clickCount').textContent = clickCount;
+        canClick = false;
+        document.getElementById('timerMessage').textContent = "Подождите 30 секунд перед следующим кликом.";
+
+        setTimeout(() => {
+            canClick = true;
+            document.getElementById('timerMessage').textContent = "";
+        }, 30000); // 30 секунд = 30000 миллисекунд
+    } else {
+        document.getElementById('timerMessage').textContent = "Подождите 30 секунд перед следующим кликом.";
+    }
 });
